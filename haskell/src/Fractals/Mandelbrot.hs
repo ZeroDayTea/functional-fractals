9539840a137ae4fe169e79731fce8513d3d29e87
ZeroDayTea <patrick.dobranowski@gmail.com>
2025-01-02 21:45:53 -0700

    haskell generating basic images

--- haskell/src/Fractals/Mandelbrot.hs
+++ haskell/src/Fractals/Mandelbrot.hs
@@ -5,7 +5,7 @@ import System.IO (withFile, IOMode(WriteMode), hPutStr)
 type Color = (Int, Int, Int)
 
 mandelbrotIters :: Double -> Int -> Double -> Double -> Int
-mandelbrotIters radius maxIter cx cy = go 0 0 0
+mandelbrotIters radius maxIter cx cy = go 0 (0, 0)
   where
     go :: Int -> (Double, Double) -> Int
     go iter (x, y)
@@ -15,8 +15,6 @@ mandelbrotIters radius maxIter cx cy = go 0 0 0
         let x' = x*x - y*y + cx
             y' = 2*x*y + cy
         in go (iter+1) (x', y')
-
-    go iter xy = go iter xy
 
 iterationToColor :: Int -> Int -> Color
 iterationToColor maxIter iter =
@@ -28,3 +26,24 @@ iterationToColor maxIter iter =
 
 generateMandelbrot :: Int -> Int -> Int -> Double -> (Double, Double) -> (Double, Double) -> FilePath -> IO()
 generateMandelbrot width height maxIter radius (xMin, xMax) (yMin, yMax) outFile = do
+  let dx = (xMax - xMin) / fromIntegral width
+      dy = (yMax - yMin) / fromIntegral height
+
+  withFile outFile WriteMode $ \handle -> do
+    hPutStr handle $ "P3\n" ++ show width ++ " " ++ show height ++ "\n255\n"
+    mapM_ (writeRow handle dx dy) [0 .. height-1]
+
+  where
+    writeRow handle dx dy row = do
+      let y0 = yMin + fromIntegral row * dy
+      let rowPixels = [ pixelColor col y0 dx | col <- [0 .. width-1] ]
+      mapM_ (writePixel handle) rowPixels
+
+    pixelColor col y0 dx =
+      let x0 = xMin + fromIntegral col * dx
+          iterCount = mandelbrotIters radius maxIter x0 y0
+          (r, g, b) = iterationToColor maxIter iterCount
+      in (r, g, b)
+
+    writePixel handle (r, g, b) =
+      hPutStr handle (show r ++ " " ++ show g ++ " " ++ show b ++ "\t")
--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -1,8 +1,16 @@
 module Main where
 
-import Fractals.Mandelbrot ()
+import Fractals.Mandelbrot (generateMandelbrot)
 
 main :: IO ()
 main = do
-  let outFile = "mandelbrot.ppm"
+  let width = 800
+      height = 600
+      radius = 2.0
+      maxIter = 512
+      xRange = (-2.0, 1.0)
+      yRange = (-1.5, 1.5)
+      outFile = "mandelbrot.ppm"
+
   putStrLn $ "Generating Mandelbrot image: " ++ outFile
+  generateMandelbrot width height maxIter radius xRange yRange outFile
